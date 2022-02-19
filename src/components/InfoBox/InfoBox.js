7f6d6df0965e1fd180298e03d194db878467913a
Hammad Basit <hammadbasithb@gmail.com>
2022-02-19 20:40:38 +0530

    Fixing Color issue

--- src/components/InfoBox/InfoBox.js
+++ src/components/InfoBox/InfoBox.js
@@ -1,6 +1,17 @@
 import React from 'react'
 
 function InfoBox(props) {
+
+  /**
+   * PurgeCSS:
+   * border-red-600
+   * border-green-600
+   * border-amber-900
+   * text-green-600
+   * text-amber-900
+   * text-red-600
+   */
+
   return (
     <div className={`card flex-1 bg-white border-t-8 border-${props.color} mr-3 p-4 rounded-md pb-6 shadow-info hover:shadow-xl transition-all`}>
       <div className='cardContent'>
